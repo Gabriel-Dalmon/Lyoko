80a78ed5aa1323d99188a7c61895a0cef0b318b4
Gabriel D. <dalmgabi@gmail.com>
2026-07-04 22:02:52 +0200

    fix: no longer drop main item on ternary interaction and fix ai attack state switch to chase

--- Source/LyokoBase/Private/Gameplay/Characters/HumanoidCharacter.cpp
+++ Source/LyokoBase/Private/Gameplay/Characters/HumanoidCharacter.cpp
@@ -10,34 +10,34 @@
 */
 void AHumanoidCharacter::Pickup_Implementation(const TScriptInterface<IPickupable>& Pickupable)
 {
-	const auto PickupableObject = Pickupable.GetObject();
-	checkf(PickupableObject, TEXT("Pickup called with an invalid Pickupable."));
+    const auto PickupableObject = Pickupable.GetObject();
+    checkf(PickupableObject, TEXT("Pickup called with an invalid Pickupable."));
 
-	// Steal Pickupable if already carried else cancel pickup
-	if (IPickupable::Execute_IsCarried(PickupableObject)) {
-		if (!IPickupable::Execute_CanBeStolen(PickupableObject, TScriptInterface<IPickuper>(this))) {
-			return;
-		} 
-		else
-		{
-			IPickupable::Execute_OnStolen(PickupableObject, TScriptInterface<IPickuper>(this));
-		}
-	}
+    // Steal Pickupable if already carried else cancel pickup
+    if (IPickupable::Execute_IsCarried(PickupableObject)) {
+        if (!IPickupable::Execute_CanBeStolen(PickupableObject, TScriptInterface<IPickuper>(this))) {
+            return;
+        } 
+        else
+        {
+            IPickupable::Execute_OnStolen(PickupableObject, TScriptInterface<IPickuper>(this));
+        }
+    }
 
-	// Drop MainItem if not empty handed
-	if (MainItem) {
-		IDropper::Execute_Drop(this, MainItem);
-		MainItem = nullptr;
-	}
+    // Drop MainItem if not empty handed
+    if (MainItem) {
+        IDropper::Execute_Drop(this, MainItem);
+        MainItem = nullptr;
+    }
 
-	// Update the Pickupable to become the new MainItem
-	MainItem = Cast<AItem>(PickupableObject);
-	if (!MainItem)
-	{
-		return;
-	}
-	MainItem->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, RightHandGripSocketName);
-	IPickupable::Execute_OnPickedUp(MainItem, this);
+    // Update the Pickupable to become the new MainItem
+    MainItem = Cast<AItem>(PickupableObject);
+    if (!MainItem)
+    {
+        return;
+    }
+    MainItem->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, RightHandGripSocketName);
+    IPickupable::Execute_OnPickedUp(MainItem, this);
 }
 
 /**
@@ -45,9 +45,9 @@ void AHumanoidCharacter::Pickup_Implementation(const TScriptInterface<IPickupabl
 */
 void AHumanoidCharacter::Drop_Implementation(const TScriptInterface<IDroppable>& Droppable)
 {
-	auto DroppableObject = Droppable.GetObject();
-	checkf(DroppableObject, TEXT("Drop called with an invalid Droppable."));
-	IDroppable::Execute_OnDropped(DroppableObject, this);
+    auto DroppableObject = Droppable.GetObject();
+    checkf(DroppableObject, TEXT("Drop called with an invalid Droppable."));
+    IDroppable::Execute_OnDropped(DroppableObject, this);
 }
 
 /**
@@ -55,8 +55,8 @@ void AHumanoidCharacter::Drop_Implementation(const TScriptInterface<IDroppable>&
 */
 void AHumanoidCharacter::OnPickupableInReach_Implementation(const TScriptInterface<IPickupable>& Pickupable)
 {
-	checkf(Pickupable.GetObject(), TEXT("OnPickupableInReach called with an invalid Pickupable."));
-	Pickupables.Add(Pickupable);
+    checkf(Pickupable.GetObject(), TEXT("OnPickupableInReach called with an invalid Pickupable."));
+    Pickupables.Add(Pickupable);
 }
 
 /**
@@ -64,8 +64,8 @@ void AHumanoidCharacter::OnPickupableInReach_Implementation(const TScriptInterfa
 */
 void AHumanoidCharacter::OnPickupableOutOfReach_Implementation(const TScriptInterface<IPickupable>& Pickupable)
 {
-	checkf(Pickupable.GetObject(), TEXT("OnPickupableOutOfReach called with an invalid Pickupable."));
-	Pickupables.RemoveSwap(Pickupable);
+    checkf(Pickupable.GetObject(), TEXT("OnPickupableOutOfReach called with an invalid Pickupable."));
+    Pickupables.RemoveSwap(Pickupable);
 }
 
 /**
@@ -73,69 +73,69 @@ void AHumanoidCharacter::OnPickupableOutOfReach_Implementation(const TScriptInte
 */
 void AHumanoidCharacter::Interact_Implementation(EInteractionTypes Type)
 {
-	switch (Type) {
-	case EInteractionTypes::Primary:
-		PrimaryInteract();
-		break;
-	case EInteractionTypes::Secondary:
-		SecondaryInteract();
-	case EInteractionTypes::Ternary:
-		TernaryInteract();
-	}
+    switch (Type) {
+    case EInteractionTypes::Primary:
+        PrimaryInteract();
+        break;
+    case EInteractionTypes::Secondary:
+        SecondaryInteract();
+    case EInteractionTypes::Ternary:
+        TernaryInteract();
+    }
 }
 
 /**
 */
 void AHumanoidCharacter::PrimaryInteract_Implementation()
 {
-	if (MainItem && MainItem->Implements<UInteractable>()) {
-		IInteractable::Execute_OnInteracted(MainItem, EInteractionTypes::Primary);
-	}
+    if (MainItem && MainItem->Implements<UInteractable>()) {
+        IInteractable::Execute_OnInteracted(MainItem, EInteractionTypes::Primary);
+    }
 }
 
 /**
 */
 void AHumanoidCharacter::SecondaryInteract_Implementation()
 {
-	// Pickup Pickupable in reach if no empty handed
-	if (!MainItem && !Pickupables.IsEmpty())
-	{
-		auto Pickupable = Pickupables.Last();
-		IPickuper::Execute_Pickup(this, Pickupable);
-		Pickupables.RemoveSwap(Pickupable);
-	}
-	else
-	{
-		//Try to interact with environment (list of IInteractables)
-	}
+    // Pickup Pickupable in reach if no empty handed
+    if (!MainItem && !Pickupables.IsEmpty())
+    {
+        auto Pickupable = Pickupables.Last();
+        IPickuper::Execute_Pickup(this, Pickupable);
+        Pickupables.RemoveSwap(Pickupable);
+    }
+    else
+    {
+        //Try to interact with environment (list of IInteractables)
+    }
 }
 
 /**
 */
 void AHumanoidCharacter::TernaryInteract_Implementation()
 {
-	//Drop MainItem or Swap with another Pickupable in reach
-	if (MainItem) {
-		if (!Pickupables.IsEmpty()) {
-			auto Pickupable = Pickupables.Last();
-			IPickuper::Execute_Pickup(this, Pickupable);
-			Pickupables.RemoveSwap(Pickupable);
-		}
-		else {
-			DropMainItem();
-		}
-	}
+    //Drop MainItem or Swap with another Pickupable in reach
+    if (MainItem) {
+        if (!Pickupables.IsEmpty()) {
+            auto Pickupable = Pickupables.Last();
+            IPickuper::Execute_Pickup(this, Pickupable);
+            Pickupables.RemoveSwap(Pickupable);
+        }
+        else {
+            //DropMainItem();
+        }
+    }
 }
 
 /**
 */
 void AHumanoidCharacter::DropMainItem_Implementation()
 {
-	if (MainItem) {
-		checkf(MainItem->Implements<UDroppable>(), TEXT("Tried to drop %s's MainItem without the %s interface."), *GetName(), *UDroppable::StaticClass()->GetName());
-		IDropper::Execute_Drop(this, MainItem);
-	}
-	MainItem = nullptr;
+    if (MainItem) {
+        checkf(MainItem->Implements<UDroppable>(), TEXT("Tried to drop %s's MainItem without the %s interface."), *GetName(), *UDroppable::StaticClass()->GetName());
+        IDropper::Execute_Drop(this, MainItem);
+    }
+    MainItem = nullptr;
 }
 
 /**
@@ -144,12 +144,12 @@ void AHumanoidCharacter::DropMainItem_Implementation()
 */
 FTransform AHumanoidCharacter::GetGrabTransform(ERelativeTransformSpace TransformSpace) const
 {
-	USkeletalMeshComponent* MeshComponent = GetMesh();
-	FName HandleSocketName = LeftHandGripSocketName;
-	if ((MeshComponent && MeshComponent->DoesSocketExist(HandleSocketName)) == false)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("%s: Socket '%s' not found on mesh!"), *AHumanoidCharacter::StaticClass()->GetName(), *HandleSocketName.ToString());
-		return FTransform();
-	}
-	return MeshComponent->GetSocketTransform(HandleSocketName, TransformSpace);
+    USkeletalMeshComponent* MeshComponent = GetMesh();
+    FName HandleSocketName = LeftHandGripSocketName;
+    if ((MeshComponent && MeshComponent->DoesSocketExist(HandleSocketName)) == false)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("%s: Socket '%s' not found on mesh!"), *AHumanoidCharacter::StaticClass()->GetName(), *HandleSocketName.ToString());
+        return FTransform();
+    }
+    return MeshComponent->GetSocketTransform(HandleSocketName, TransformSpace);
 }
