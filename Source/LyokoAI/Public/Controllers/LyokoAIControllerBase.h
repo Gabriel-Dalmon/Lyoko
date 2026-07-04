618ed7448b8eb9e878bfab1defa10c8f2dccb201
Gabriel D. <dalmgabi@gmail.com>
2026-07-05 00:16:53 +0200

    fix: ai ranged attack behavior and death collisions

--- Source/LyokoAI/Public/Controllers/LyokoAIControllerBase.h
+++ Source/LyokoAI/Public/Controllers/LyokoAIControllerBase.h
@@ -6,6 +6,9 @@
 #include "AIController.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "BehaviorTree/Blackboard/BlackboardKeyType.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "Perception/AISenseConfig_Hearing.h"
 #include "LyokoAIControllerBase.generated.h"
 
 /**
@@ -15,6 +18,16 @@ UCLASS(Abstract)
 class LYOKOAI_API ALyokoAIControllerBase : public AAIController
 {
     GENERATED_BODY()
+
+public:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI|Perception")
+    UAIPerceptionComponent *PerceptionComponentBis;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AI|Perception")
+    UAISenseConfig_Sight *SightConfig;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AI|Perception")
+    UAISenseConfig_Hearing *HearingConfig;
 
 public:
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "LyokoAI")
@@ -27,11 +40,19 @@ public:
     FName BehaviorStateKey = TEXT("State");
 
 public:
+    ALyokoAIControllerBase();
+
+    virtual void OnPossess(APawn *InPawn) override;
+
+public:
     UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "LyokoAI|Controller")
     void OnBehaviourStateChanged(FName ChangedKeyName);
     virtual void OnBehaviourStateChanged_Implementation(FName ChangedKeyName);
 
     EBlackboardNotificationResult InternalOnBehaviourStateChanged(const UBlackboardComponent &BlackboardComp, FBlackboard::FKey ChangedKeyID);
 
-    virtual void OnPossess(APawn *InPawn) override;
+
+public:
+    UFUNCTION(BlueprintCallable, Category = "AI|Perception")
+    void SetHearingRadius(float Radius);
 };
